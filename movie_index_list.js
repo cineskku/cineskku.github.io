e3bdf6024fe7349c93201ba0949f97ad9cf5d2dc
cineskku <34303454+cineskku@users.noreply.github.com>
2018-12-04 18:27:39 +0900

    Add files via upload

--- movie_index_list.js
+++ movie_index_list.js
@@ -64,13 +64,13 @@ function movie_list() {
   xhttp.send(null);
   var objDoc = xhttp.responseXML;
 
-  array_list = objDoc.childNodes.length;
+  array_list = objDoc.getElementsByTagName("movie_index").length;
 
   for (var i = 0; i < array_list; i++) {
 
     var urlLink = document.createElement("a");
       urlLink.setAttribute("href", objDoc.getElementsByTagName("movie_index")[i].getElementsByTagName("movie")[0].childNodes[0].nodeValue);
-      urlLink.createTextNode(objDoc.getElementsByTagName("movie_index")[i].getElementsByTagName("movie")[1].childNodes[0].nodeValue);
+      urlLink.appendChild(document.createTextNode(objDoc.getElementsByTagName("movie_index")[i].getElementsByTagName("movie")[1].childNodes[0].nodeValue));
 
     var ul = document.getElementById("introduce_for_writexml");
       ul.appendChild(urlLink);
